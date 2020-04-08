650108916abe500542fab50d277eca2f8fa6c70c
root <root@localhost.localdomain>
2020-04-08 10:42:56 +0000

    enum base class

---
+++ Abstracts/Enum.php
@@ -0,0 +1,14 @@
+<?php
+namespace SeaDrip\Abstracts;
+
+abstract class Enum
+{
+    public final static function has( $val ) : bool {
+        return in_array( $val, array_values( self::_constants() ) );
+    }
+
+    private final static function _constants() : array {
+        return ( new ReflectionClass( static::class ) )->getConstants();
+    }
+}
+
