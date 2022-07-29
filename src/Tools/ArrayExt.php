6d48ae6d9203fa124f3b39e8968de65246241a23
SDPoplar <741027037@qq.com>
2022-07-29 17:14:56 +0800

    update some tools

--- src/Tools/ArrayExt.php
+++ src/Tools/ArrayExt.php
@@ -3,9 +3,10 @@ namespace SeaDrip\Tools;
 
 abstract class ArrayExt
 {
-    public static function pick_key( array $origin, array $keys ) : array {
-        $mask = array_combine( $keys, array_pad( [], count( $keys ), 0 ) );
-        return array_intersect_key( $origin, $mask );
+    public static function pick_key( array $origin, array $keys ) : array
+    {
+        $mask = array_combine($keys, array_pad([], count( $keys ), 0));
+        return array_intersect_key($origin, $mask);
     }
 }
 
---
+++ src/Tools/EfficiencyComparer.php
@@ -0,0 +1,37 @@
+<?php
+namespace Mxs\Tools;
+
+class EfficiencyComparer
+{
+    function result($wanted, ...$args)
+    {   
+        foreach ($this->method_dict as $method_name => $method) {
+            $result = $method(...$args);
+            if ($result === $wanted) {
+                continue;
+            }   
+            echo 'result of '.$method_name.':'.PHP_EOL;
+            var_dump($result);
+            echo PHP_EOL;
+        }   
+    }
+
+    function speed(int $times, ...$args)
+    {   
+        foreach ($this->method_dict as $method_name => $method) {
+            $ts_start = time();
+            for ($i=0; $i < $times; $i++) {
+                $a = $method(...$args);
+            }   
+            echo "{$method_name} run {$times} time(s) in ".(time() - $ts_start).' second(s)'.PHP_EOL;
+        }   
+    }   
+
+    public function __construct(array $method_dict)
+    {   
+        $this->method_dict = $method_dict;
+    }   
+
+    protected readonly array $method_dict;
+}
+
