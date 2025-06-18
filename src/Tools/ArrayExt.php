942267cef4b64250d6395f98e03ccc9e8c9c77e7
SDPoplar <741027037@qq.com>
2025-06-18 14:11:13 +0800

    fix link format

--- src/Tools/ArrayExt.php
+++ src/Tools/ArrayExt.php
@@ -13,5 +13,31 @@ abstract class ArrayExt
     {
         return array_combine(array_column($origin, $key_column), $origin);
     }
+
+    public static function toFile(array $content, Path|string $save_to): bool
+    {
+        if (!is_writable(dirname($save_to))) {
+            return false;
+        }
+        $str = '<?php'.PHP_EOL.'return '.self::itemToStringRecursive($content).';';
+        return file_put_contents($save_to, $str);
+    }
+
+    private static function itemToStringRecursive(array $arr, int $tabSpace = 0): string
+    {
+        $lines = ['['];
+        $prefix_space = str_repeat(' ', $tabSpace + 4);
+        foreach ($arr as $key => $val) {
+            $val_str = match(gettype($val)) {
+                'array' => self::itemToStringRecursive($val, $tabSpace + 4),
+                'string' => "'{$val}'",
+                //  TODO: other types, e.g. object/class
+                default => $val
+            };
+            $lines[] = "{$prefix_space}'{$key}' => {$val_str},";
+        }
+        $lines[] = str_repeat(' ', $tabSpace).']';
+        return implode(PHP_EOL, $lines);
+    }
 }
 
