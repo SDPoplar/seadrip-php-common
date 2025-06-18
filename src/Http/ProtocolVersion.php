47ea7e3f80b28666e7066e0e08a4981819d6c259
SDPoplar <741027037@qq.com>
2025-06-18 13:32:11 +0800

    add enum \SeaDrip\Http\ProtocolVersion it's the no-prefix 'HTTP/' version for enum \SeaDrip\Http\Protocol it's made to use for Psr-7 MessageInterface

---
+++ src/Http/ProtocolVersion.php
@@ -0,0 +1,21 @@
+<?php
+namespace SeaDrip\Http;
+
+/**
+ * Http version number
+ * almost the same like enum Protocal, but without prefix 'HTTP/'
+ * this only number version protocal is made for PSR-7: HTTP message interfaces
+ * MessageInterface::getProtocalVersion() prescribed that the method
+ *      MUST contain only the HTTP version number (e.g., "1.1", "1.0")
+ * so this why we defined enum ProtocolVersion when we already have enum Protocal
+ * 
+ * Psr-7:
+ * @link https://www.php-fig.org/psr/psr-7/
+ */
+enum ProtocolVersion: string
+{
+    case V0_9 = '0.9';
+    case V1_0 = '1.0';
+    case V1_1 = '1.1';
+    case V2   = '2';
+}
